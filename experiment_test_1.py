5c972a3b56be95b3ee88f1e8d047679b23563a29
Sulagna Nandi <sulagna.nandi.12@gmail.com>
2026-08-15 20:23:12 -0400

    experiment test 1

--- experiment_test_1.py
+++ experiment_test_1.py
@@ -7,9 +7,23 @@ class WeaklySelfAvoidingWalk:
     max_steps = 1000
     cols = 10
     rows = 10
-    grid = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    def __init__(step_count, max_steps, grid, weakener, spacing, offset):
+    def __init__(self, step_count, max_steps, weakener, spacing):
+        global cols, rows
+        self.grid = [[0 for _ in range(cols)] for _ in range(rows)]
+        self.weakener = weakener
+        self.spacing = spacing
+        self.offset = spacing // 2
+
+class WSAWRunner:
+
+    def __init__(self):
+        print("hi")
+
+    def main_runner():
+        print("hi")
+
+    def one_full_walk():
         print("hi")
 
     
