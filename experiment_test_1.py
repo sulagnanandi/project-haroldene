394eb7e1640666572471d806fc00341f5dabc6bf
Sulagna Nandi <sulagna.nandi.12@gmail.com>
2026-08-15 20:08:41 -0400

    hi

---
+++ experiment_test_1.py
@@ -0,0 +1,15 @@
+import py5
+import numpy as np
+import random
+
+class WeaklySelfAvoidingWalk:
+
+    max_steps = 1000
+    cols = 10
+    rows = 10
+    grid = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    def __init__(step_count, max_steps, grid, weakener, spacing, offset):
+        print("hi")
+
+    
